066d712399c682b6c89559222157c8f1cbde058b
VivekAlhat <vivekalhat14@outlook.com>
2021-06-17 02:17:40 +0530

    Render Upvotes From DB

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -2,11 +2,24 @@ import Articles from "../components/Articles";
 import Error from "./Error";
 import content from "../data/content";
 import "./css/article.css";
+import { useEffect, useState } from "react";
 
 const Article = ({ match }) => {
   const name = match.params.name;
   const article = content.find((item) => item.name === name);
   const otherArticles = content.filter((item) => item.name !== name);
+
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    const getData = async () => {
+      const result = await fetch(`/api/${name}`);
+      const body = await result.json();
+      console.log(body);
+      setArticleInfo(body);
+    };
+    getData();
+  }, [name]);
 
   if (!article) {
     return <Error />;
@@ -15,6 +28,7 @@ const Article = ({ match }) => {
   return (
     <div className="article-container">
       <h1 id="name">{article.title}</h1>
+      <p>This article has been liked by {articleInfo.upvotes} people</p>
       <p id="description">{article.article}</p>
       <div className="related">
         <h3 className="related-title">Related Articles:</h3>
