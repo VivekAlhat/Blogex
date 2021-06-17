339537c949ecc70fcd8bdd9c5268ebbe3f473c87
VivekAlhat <vivekalhat14@outlook.com>
2021-06-17 15:08:59 +0530

    Add CORS

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors');
 const { MongoClient } = require("mongodb");
 
 const DB_URI = "mongodb://localhost:27017";
 
 const app = express();
+app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
